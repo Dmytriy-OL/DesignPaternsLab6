174fdeb03c597072a003884209d1fa7786be2910
MaksymRybak <vt221_rmo@student.ztu.edu.ua>
2024-05-04 19:59:54 +0300

    find: page elements

---
+++ app/src/main.ts
@@ -0,0 +1,6 @@
+const recordBar = document.querySelector('.record-indicator span');
+const progressBar = document.querySelector('.progress-indicator span');
+
+const mainTitleWord = document.querySelector('.main__title span');
+const taskAnswerButtons = document.querySelectorAll('.task-block__answer');
+const languageModeButton = document.querySelector('.chenge-language-mode-block__element');
